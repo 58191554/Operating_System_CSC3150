dce907fb37b76b26aaa364dd8b1aa776b6b32869
Zhen Tong <88566085+58191554@users.noreply.github.com>
2022-10-22 14:08:20 +0800

    Update async.h

--- HM_2/3150-p2-bonus-main/thread_poll/async.h
+++ HM_2/3150-p2-bonus-main/thread_poll/async.h
@@ -3,13 +3,14 @@
 
 #include <pthread.h>
 
+
 // the task struct
 typedef struct my_item {
 
     struct my_item *next;
     struct my_item *prev;
     /* TODO */
-    void (*taskFunction)(int x);
+    void (*taskFunction)(int);
     int args;
 
 } my_item_t;
@@ -18,9 +19,13 @@ typedef struct my_queue {
 
     int size;
     my_item_t *head;
+    my_item_t *tail;
     /* TODO */
 } my_queue_t;
 
+
+void enqueue(my_queue_t *queue, my_item_t *item);
+my_item_t * dequeue(my_queue_t *queue);
 void async_init(int);
 void async_run(void (*fx)(int), int args);
 
