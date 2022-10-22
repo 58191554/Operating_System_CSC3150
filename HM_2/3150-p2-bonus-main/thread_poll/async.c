7fd0899291d26991394abd0b55022db2ee90e688
Zhen Tong <88566085+58191554@users.noreply.github.com>
2022-10-22 14:08:04 +0800

    Update async.c

--- HM_2/3150-p2-bonus-main/thread_poll/async.c
+++ HM_2/3150-p2-bonus-main/thread_poll/async.c
@@ -3,75 +3,90 @@
 #include "async.h"
 #include "utlist.h"
 
-
-// thread_safe_queue 
-struct my_queue work_queue;
-int THREAD_NUM;
-
-// create an array of threads
+// an array of threads
 pthread_t * th;
 pthread_mutex_t mutexQueue;
+// a queue of tasks
+my_queue_t workQueue ;
+my_queue_t *workQueuePt = &workQueue;
 
-my_item_t taskQueue[5000];
-
-
-void submitTask(my_item_t task){
+void submitTask(my_item_t *task){
     pthread_mutex_lock(&mutexQueue);
-    taskQueue[taskCount] = task;
-    taskCount ++;
+    enqueue(workQueuePt, task);
+    printf("the queue size is %d\n", workQueue.size);
     pthread_mutex_unlock(&mutexQueue);
 }
 
+void enqueue(my_queue_t *queue, my_item_t *item){
+    if(queue->size == 0){
+        queue->head = item;
+    }
+    else{
+        queue->tail->next = item;
+        queue->tail = item;
+    }
+    queue->size ++;
+}
+
+my_item_t * dequeue(my_queue_t *queue){
+    if(queue->size == 0){
+        return NULL;
+    }
+    else{
+        int last = queue->tail;
+        queue->tail = queue->tail->prev;
+        return last;
+    }
+    queue->size --;
+}
+
+void executeTask(my_item_t * task){
+    printf("task = %d", task->taskFunction);
+    printf("args = %d", task->args);
+    task->taskFunction(task->args);
+}
 
 void* startThread(void* args){
     while(1){
         my_item_t *task;
 
         pthread_mutex_lock(&mutexQueue);
-        if(taskCount > 0){
-            task = &taskQueue[0];
-            for(int i = 0; i < taskCount-1; i++){
-                taskQueue[i] = taskQueue[i+1];
-            }
-            taskCount --;
+        if(workQueuePt->size > 0){
+            printf("myQueue size = %d", workQueuePt->size);
+            task = dequeue(workQueuePt->head);
+            workQueuePt->size--;
         }
         pthread_mutex_unlock(&mutexQueue);
-
+        executeTask(task);
     }
 }
 
 void async_init(int num_threads) {
     /** TODO: create num_threads threads and initialize the thread pool **/
-
+    workQueue.head = NULL;
+    workQueue.size = 0;
     pthread_mutex_init(&mutexQueue, NULL);
-
-
-    THREAD_NUM = num_threads;
     pthread_t th[num_threads];
     // need a array to store the thread in the thread pool
     int i;
+
     for (i = 0; i < num_threads; i++){
         if(pthread_create(&th[i], NULL, &startThread, NULL) != 0){
             perror("Failed to create the thread");
         }
     }
-    // // a joiner object to join
 
     return ;
 }   
 
 void async_run(void (*hanlder)(int), int args) {
-
-    taskCount++;
-    // printf("now we have %d jobs to do%d\n", taskCount, &taskCount);
-    int i;
+    // submitTask the task my_item_t t
     my_item_t t = {
         .taskFunction = hanlder,
         .args = args
     };
 
-    // submitTask the task my_item_t t
-    submitTask(t);
+    submitTask(&t);
 
     return ;
 }
