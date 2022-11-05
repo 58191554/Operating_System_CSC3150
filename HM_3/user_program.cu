806837b8766ebc7bf0ae7b9f755c99123da7d1ab
Zhen Tong <88566085+58191554@users.noreply.github.com>
2022-11-05 11:40:50 +0800

    Update user_program.cu

--- HM_3/user_program.cu
+++ HM_3/user_program.cu
@@ -1,14 +1,36 @@
-﻿#include "virtual_memory.h"
+#include "virtual_memory.h"
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <stdio.h>
 
-__device__ void user_program(VirtualMemory *vm, uchar *input, uchar *results,
-                             int input_size) {
-  for (int i = 0; i < input_size; i++)
-    vm_write(vm, i, input[i]);
+// __device__ void user_program(VirtualMemory *vm, uchar *input, uchar *results,
+//                              int input_size) {
+// 	printf("testing program begin\n");
 
-  for (int i = input_size - 1; i >= input_size - 32769; i--)
-    int value = vm_read(vm, i);
+// 	for(int i = 0; i < input_size;i++)
+// 		vm_write(vm, i, input[i]);
+	
+// 	printf("part1 done\n");
 
-  vm_snapshot(vm, results, 0, input_size);
+//   	for (int i = input_size - 1; i >= 0; i--)
+//     	int value = vm_read(vm, i);
+
+//   	for (int i = input_size - 1; i >= input_size - 32769; i--)
+//     	int value = vm_read(vm, i);
+
+//   	vm_snapshot(vm, results, 0, input_size);
+// 	printf("testing program end\n");
+// }
+
+__device__ void user_program(VirtualMemory *vm, uchar *input, uchar *results,int input_size) {
+	// write the data.bin to the VM starting from address 32*1024
+	for (int i = 0; i < input_size; i++)
+	  	vm_write(vm, 32*1024+i, input[i]);
+	// write (32KB-32B) data  to the VM starting from 0
+	for (int i = 0; i < 32*1023; i++)
+	  	vm_write(vm, i, input[i+32*1024]);
+	// readout VM[32K, 160K] and output to snapshot.bin, which should be the same with data.bin
+	vm_snapshot(vm, results, 32*1024, input_size);
 }
+
+// expected page fault num: 9215
